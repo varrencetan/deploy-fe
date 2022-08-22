bf944a268eacd8aaf1da0aeed7a0784d648ecc4c
Taufik <profesorgreen36@gmail.com>
2022-08-22 20:00:52 +0700

    try conn db

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -5,4 +5,11 @@ const db = new Sequelize('people','people','people',{
     dialect: 'mysql'
 });
 
+try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+  
 export default db;
