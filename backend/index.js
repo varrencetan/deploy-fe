a128d18dda7704ee65b1066afe36213ec5bc09d5
Taufik <profesorgreen36@gmail.com>
2022-08-29 12:35:31 +0700

    edit .env file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
+import dotenv from "dotenv";
+
+// load .env
+dotenv.config()
+
+const BASE_URL_PORT = process.env.BASE_URL_PORT
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
-app.listen(5000, ()=> console.log('Server up and running...'));
+app.listen(BASE_URL_PORT, ()=> console.log('Server up and running...'));
